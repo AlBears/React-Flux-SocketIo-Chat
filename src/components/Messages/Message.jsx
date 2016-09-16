c1cb331960a78a1c04598e0307045035bdf90899
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-16 12:44:29 +0300

    Username functionality

--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -6,10 +6,10 @@ import moment from 'moment';
 class Message extends Component{
 	render(){
     const {message} = this.props;
-    var formattedTime = moment(message.timeStamp).format('h:mm a')
+    var formattedTime = moment(message.timeStamp).format('h:mm:ss a')
 		return(
 			<div className="message">
-				<strong></strong>{formattedTime} - {message.text}
+				<strong>{message.user}</strong> {formattedTime} - {message.text}
 			</div>
 		)
 	}
